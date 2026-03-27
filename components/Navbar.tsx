1db95d180e79e48e47fb9912e4cbc04953c35926
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2026-03-27 17:44:04 +0500

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,39 @@
+import { Box } from "lucide-react"
+
 const NavBar = () => {
+  const handleAuthClick = async () => {};
+  
   return (
-    <div>NavBar</div>
+    <header className="navbar" >
+      <nav className="inner">
+        <div className="left">
+          <div className="brand">
+            <Box className="logo" />
+
+            <span className="name">
+              Roomification
+            </span>
+            
+          </div>
+          <ul className="links">
+            <a href="#">Products</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+        <div className="actions">
+            <button  onClick={handleAuthClick}
+            className="login">
+              Log In
+            </button>
+            <a href="#uploads" className="cta">
+              Get Started
+            </a>
+        </div>
+
+      </nav>
+    </header>
   )
 }
 
