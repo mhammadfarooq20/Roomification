4dab59d4c6be2c807716978cd25f80fc79f784eb
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2026-03-27 21:17:42 +0500

    hero

--- app/root.tsx
+++ app/root.tsx
@@ -86,7 +86,7 @@ export default function App() {
   };
   return (
     <main className="min-h-screen bg-background text-foreground relative z-10 ">
-      <Outlet context={{ ...authState, refreshAuth, signIn, signOut }} />;
+      <Outlet context={{ ...authState, refreshAuth, signIn, signOut }} />
     </main>
   );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
-import NavBar from "components/NavBar";
+import NavBar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +13,30 @@ export default function Home() {
   return (
     <div className="home">
       <NavBar />
-      <h1 className="text-3xl text-indigo-700 font-extrabold ">Hello</h1>
+
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+          <p>Introducing Roomification</p>
+        </div>
+
+        <h1>
+          Build beautiful spaces at the speed of thought with Roomification
+        </h1>
+        <p className="subtitle">
+          Roomification is the AI-First design environment that helps you
+          visualize,render, and ship architectural projects faster than
+          ever
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
