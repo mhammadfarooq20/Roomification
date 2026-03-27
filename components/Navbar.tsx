5113253d2e999cc5a99f48f21392b1f273965114
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2026-03-27 18:12:23 +0500

    Button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,19 @@
-import { Box } from "lucide-react"
+import { Box } from "lucide-react";
+import Button from "./ui/Button";
 
 const NavBar = () => {
+  const isSignedIn = false;
+  const userName = "hammad";
   const handleAuthClick = async () => {};
-  
+
   return (
-    <header className="navbar" >
+    <header className="navbar">
       <nav className="inner">
         <div className="left">
           <div className="brand">
             <Box className="logo" />
 
-            <span className="name">
-              Roomification
-            </span>
-            
+            <span className="name">Roomification</span>
           </div>
           <ul className="links">
             <a href="#">Products</a>
@@ -23,18 +23,29 @@ const NavBar = () => {
           </ul>
         </div>
         <div className="actions">
-            <button  onClick={handleAuthClick}
-            className="login">
-              Log In
-            </button>
-            <a href="#uploads" className="cta">
-              Get Started
-            </a>
+          {isSignedIn ? (
+            <>
+              <span className="greetings">
+                {userName ? `Hi, ${userName}` : "Signed in"}
+              </span>
+              <Button size="sm" onClick={handleAuthClick} className="btn" >
+                Log Out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={handleAuthClick} size="sm" variant="ghost">
+                Log In
+              </Button>
+              <a href="#uploads" className="cta">
+                Get Started
+              </a>
+            </>
+          )}
         </div>
-
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
