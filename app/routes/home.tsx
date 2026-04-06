f3fda87c54c706993fe05eb79c2d24386a934ae3
mhammadfarooq20 <muhammad.hammad.farooq.02@gmail.com>
2026-04-06 20:25:06 +0500

    hero

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import NavBar from "components/Navbar";
 import type { Route } from "./+types/home";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Layers } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -27,16 +28,60 @@ export default function Home() {
         </h1>
         <p className="subtitle">
           Roomification is the AI-First design environment that helps you
-          visualize,render, and ship architectural projects faster than
-          ever
+          visualize,render, and ship architectural projects faster than ever
         </p>
 
         <div className="actions">
           <a href="#upload" className="cta">
             Start Building <ArrowRight className="icon" />
           </a>
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+              <h3 className="upload-title">Upload your floor plan</h3>
+              <p>Supports JPG, PNG formats up to 10MB</p>
+            </div>
+            <p>Upload Images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects, all in one
+                place.
+              </p>
+            </div>
+          </div>
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview" />
+              <img
+                src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                alt="Project Preview"
+              />
+              <div className="badge">
+                <span>Community</span>
+              </div>
+            </div>
+            <div className="card-body">
+              
+            </div>
+          </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
